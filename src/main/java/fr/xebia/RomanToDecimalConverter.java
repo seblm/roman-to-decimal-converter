4ed13b5eb7e5250c41650882f1ce4e558ef64c50
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-06-19 19:21:01 +0200

    zero

--- src/main/java/fr/xebia/RomanToDecimalConverter.java
+++ src/main/java/fr/xebia/RomanToDecimalConverter.java
@@ -1,4 +1,7 @@
 package fr.xebia;
 
 public class RomanToDecimalConverter {
+    public static int convert(String roman) {
+        return 0;
+    }
 }
--- src/test/java/fr/xebia/RomanToDecimalConverterTest.java
+++ src/test/java/fr/xebia/RomanToDecimalConverterTest.java
@@ -1,4 +1,14 @@
 package fr.xebia;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RomanToDecimalConverterTest {
+    @Test
+    public void should_convert_zero() {
+        int decimal = RomanToDecimalConverter.convert("");
+
+        assertThat(decimal).isEqualTo(0);
+    }
 }
